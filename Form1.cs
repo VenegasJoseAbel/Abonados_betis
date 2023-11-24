956ba5e69563eefa61c8acaa7e782dbb4ab55734
VenegasJoseAbel <147171883+VenegasJoseAbel@users.noreply.github.com>
2023-11-24 10:00:41 +0100

    Actualizacion con comentarios Todo funcional

--- Form1.cs
+++ Form1.cs
@@ -2,21 +2,21 @@ namespace Abonados_betis2
 {
     public partial class Form1 : Form
     {
-        List<Abonados> Socio;
-        int posicion = 0;
+        List<Abonados> Socio; //declaro una lista que guardara los registros.
+        int posicion = 0; //declaro un entero para recorrer la lista.
 
 
         public Form1()
         {
             InitializeComponent();
             Socio = new List<Abonados>();
-            leerFichero();
-            comprobarbotones();
+            leerFichero(); //llamo al metodo para leer.
+            comprobarbotones(); //llamo a un metodo para activar/desactivar los botones anterior y siguiente.
         }
 
         public void MostrarSiguienteElemento()
         {
-            if (posicion < Socio.Count - 1)
+            if (posicion < Socio.Count - 1) //siempre que la posicion sea menor al tamaño actualiza los valores a mostrar.
             {
                 txtNum.Text = Socio[posicion + 1].numeroSocio.ToString();
                 txtNombre.Text = Socio[posicion + 1].nombreSocio;
@@ -27,12 +27,12 @@ namespace Abonados_betis2
                 ckRealizoElPago.Checked = Socio[posicion + 1].pagoSocio;
                 txtImagen.Text = Socio[posicion + 1].rutaImagenSocio.ToString();
                 posicion++;
-                cargarImagen();
-                comprobarbotones();
+                cargarImagen(); //llamo a un metodo para cargar la imagen de este registro.
+                comprobarbotones(); //llamo a un metodo para activar/desactivar los botones anterior y siguiente.
             }
         }
 
-        public void TxtBlancos()
+        public void TxtBlancos() //actualizo todos los objetos del mi formilario para pornerlo en blanco.
         {
             txtNum.Text = "";
             txtNombre.Text = "";
@@ -45,7 +45,7 @@ namespace Abonados_betis2
             ckRealizoElPago.Checked = false;
         }
 
-        public void MostrarActual()
+        public void MostrarActual() //muestro el elemento en el que se encuentre mi Lista.
         {
             txtNum.Text = Socio[posicion].numeroSocio.ToString();
             txtNombre.Text = Socio[posicion].nombreSocio;
@@ -55,55 +55,55 @@ namespace Abonados_betis2
             txtPago.Text = Socio[posicion].costoSocio.ToString();
             ckRealizoElPago.Checked = Socio[posicion].pagoSocio;
             txtImagen.Text = Socio[posicion].rutaImagenSocio.ToString();
-            cargarImagen();
-            comprobarbotones();
+            cargarImagen(); //llamo a un metodo para cargar la imagen de este registro.
+            comprobarbotones(); //llamo a un metodo para activar/desactivar los botones anterior y siguiente.
         }
 
-        private void butCrear_Click(object sender, EventArgs e)
+        private void butCrear_Click(object sender, EventArgs e) //Cuando se hace click en el boton.
         {
-            TxtBlancos();
-            lblNumeroTotalSocio.Text = Socio.Count.ToString();
+            TxtBlancos(); //llamo al metodo para dejar todos los elemtos vacios.
+            lblNumeroTotalSocio.Text = Socio.Count.ToString(); //Actualizo el contador al numero de registros que haya.
         }
 
         private void butEliminar_Click(object sender, EventArgs e)
         {
-            Socio.Remove(Socio[posicion]);
-            lblNumeroTotalSocio.Text = Socio.Count.ToString();
-            if(Socio.Count > 0 && posicion > 0)
+            Socio.Remove(Socio[posicion]); //Elimino el que se esta mostrando actualmente.
+            lblNumeroTotalSocio.Text = Socio.Count.ToString(); //Actualizo el contador al numero de registros que haya.
+            if (Socio.Count > 0 && posicion > 0) //Si mi lista tiene mas de 0 registros y no esta en la primera posicion.
             {
-                posicion--;
-                MostrarActual();
-                butEliminar.Enabled = true;
+                posicion--; //Muevo la posicion uno atras.
+                MostrarActual(); //LLamo al metodo para actualizar los valores al actual.
+                butEliminar.Enabled = true; //Activo el boton.
             }
             else
             {
-                if (posicion == 0 && Socio.Count > 0)
+                if (posicion == 0 && Socio.Count > 0) //sai esta en la primear posicion y no es el unico en mi lista.
                 {
-                    MostrarActual();
-                    butEliminar.Enabled = true;
+                    MostrarActual(); //Muestro el actual.
+                    butEliminar.Enabled = true; //Activo el boton.
                 }
-                else
+                else //Por el contrario si es el ultimo registro exixtente.
                 {
-                    butModificar.Enabled = false;
-                    butEliminar.Enabled = false;
-                    TxtBlancos();
+                    butModificar.Enabled = false; //Desactivo el boton modificar (ya que no hay mas registros).
+                    butEliminar.Enabled = false; //Desactivo el boton eliminar.
+                    TxtBlancos(); //llamo al metodo para dejar todos los elemtos vacios.
                 }
             }
-            
-            comprobarbotones();
+
+            comprobarbotones(); //llamo a un metodo para activar/desactivar los botones anterior y siguiente.
         }
 
         private void butSiguiente_Click(object sender, EventArgs e)
         {
-            MostrarSiguienteElemento();
-            lblNumeroTotalSocio.Text = Socio.Count.ToString();
+            MostrarSiguienteElemento(); //Llamo al metodo para mostrar el siguiente elemento de mi lista.
+            lblNumeroTotalSocio.Text = Socio.Count.ToString(); //Actualizo el contador al numero de registros que haya.
         }
 
-        private void butConfirmar_Click(object sender, EventArgs e)
+        private void butConfirmar_Click(object sender, EventArgs e) //Este boton lo uso para crear el nuevo registro.
         {
-            butModificar.Enabled = true;
-            butEliminar.Enabled = true;
-            Abonados abonados = new Abonados();
+            butModificar.Enabled = true; //Activo el boton de modificar (ya que añado un nuevo reguistro)
+            butEliminar.Enabled = true;  //Activo el boton de eliminar (ya que añado un nuevo reguistro)
+            Abonados abonados = new Abonados(); //creo y le doy los valores necesarios que se leen de los TextBox
             abonados.numeroSocio = int.Parse(txtNum.Text);
             abonados.nombreSocio = txtNombre.Text;
             abonados.apellidoSocio = txtApellido.Text;
@@ -114,20 +114,20 @@ namespace Abonados_betis2
             abonados.rutaImagenSocio = txtImagen.Text;
             abonados.imagenSocio = imageToByteArray(Image.FromFile(txtImagen.Text));
 
-            Socio.Add(abonados);
-            MostrarActual();
-            lblNumeroTotalSocio.Text = Socio.Count.ToString();
+            Socio.Add(abonados); //Añado mi nuevo registro.
+            MostrarActual(); //Muestro el actual.
+            lblNumeroTotalSocio.Text = Socio.Count.ToString(); //Actualizo el contador al numero de registros que haya.
         }
 
         private void butCancelar_Click(object sender, EventArgs e)
         {
-            MostrarActual();
-            lblNumeroTotalSocio.Text = Socio.Count.ToString();
+            MostrarActual(); //Muestro el actual.
+            lblNumeroTotalSocio.Text = Socio.Count.ToString();//Actualizo el contador al numero de registros que haya.
         }
 
         private void butAnterior_Click(object sender, EventArgs e)
         {
-            if (posicion > 0)
+            if (posicion > 0) //siempre que la posicion sea mayor que 0 actualiza los valores a mostrar.
             {
                 txtNum.Text = Socio[posicion - 1].numeroSocio.ToString();
                 txtNombre.Text = Socio[posicion - 1].nombreSocio;
@@ -138,19 +138,19 @@ namespace Abonados_betis2
                 ckRealizoElPago.Checked = Socio[posicion - 1].pagoSocio;
                 txtImagen.Text = Socio[posicion - 1].rutaImagenSocio.ToString();
                 posicion--;
-                cargarImagen();
-                comprobarbotones();
+                cargarImagen(); //llamo a un metodo para cargar la imagen de este registro.
+                comprobarbotones(); //llamo a un metodo para activar/desactivar los botones anterior y siguiente.
             }
-            lblNumeroTotalSocio.Text = Socio.Count.ToString();
+            lblNumeroTotalSocio.Text = Socio.Count.ToString(); //Actualizo el contador al numero de registros que haya.
         }
 
         private void butCargarImg_Click(object sender, EventArgs e)
         {
-            cargarImagen();
+            cargarImagen(); //llamo a un metodo para cargar la imagen de este registro.
         }
 
-        private void butModificar_Click(object sender, EventArgs e)
-        {
+        private void butModificar_Click(object sender, EventArgs e) //Modifico el registro actual.
+        {   
             Socio[posicion].numeroSocio = int.Parse(txtNum.Text);
             Socio[posicion].nombreSocio = txtNombre.Text;
             Socio[posicion].apellidoSocio = txtApellido.Text;
@@ -160,18 +160,18 @@ namespace Abonados_betis2
             Socio[posicion].pagoSocio = ckRealizoElPago.Checked;
             Socio[posicion].rutaImagenSocio = txtImagen.Text;
             Socio[posicion].imagenSocio = imageToByteArray(Image.FromFile(txtImagen.Text));
-            cargarImagen();
-            MostrarActual();
+            cargarImagen(); //llamo a un metodo para cargar la imagen de este registro.
+            MostrarActual(); //llamo a un metodo para activar/desactivar los botones anterior y siguiente.
         }
 
-        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        public byte[] imageToByteArray(System.Drawing.Image imageIn) //Se convierte una imagen a un Array de Byte.
         {
             MemoryStream ms = new MemoryStream();
             imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
             return ms.ToArray();
         }
 
-        public Image byteArrayToImage(byte[] byteArrayIn)
+        public Image byteArrayToImage(byte[] byteArrayIn) //Se convierte una Array de Byte a una imagen.
         {
             MemoryStream ms = new MemoryStream(byteArrayIn);
             Image returnImage = Image.FromStream(ms);
@@ -182,20 +182,20 @@ namespace Abonados_betis2
         {
             try
             {
-                imagen.Image = (byteArrayToImage(Socio[posicion].imagenSocio));
+                imagen.Image = (byteArrayToImage(Socio[posicion].imagenSocio)); //Convierto el Array de Byte y se muestra.
             }
             catch (Exception ex) { }
         }
 
         public void gardarEnFichero()
         {
-            BinaryWriter fichero;
+            BinaryWriter fichero; //Se crea un fichero para escribir.
             try
             {
-                fichero = new BinaryWriter(File.Open("databank.data", FileMode.Create));
-                fichero.Write(Socio.Count);
+                fichero = new BinaryWriter(File.Open("databank.data", FileMode.Create)); //Abrimos el fichero si exite, sino lo crea. Si ya existe lo sobre escribe.
+                fichero.Write(Socio.Count); //Guardo el numero de resgitros de mi lista.
 
-                for (int i = 0; i < Socio.Count; i++)
+                for (int i = 0; i < Socio.Count; i++) //Escribo en el fichero todo lo que vayas a usar.
                 {
                     fichero.Write(Socio[i].numeroSocio);
                     fichero.Write(Socio[i].nombreSocio);
@@ -205,21 +205,21 @@ namespace Abonados_betis2
                     fichero.Write(Socio[i].costoSocio);
                     fichero.Write(Socio[i].pagoSocio);
                     fichero.Write(Socio[i].rutaImagenSocio);
-                    fichero.Write(Socio[i].tam);
+                    fichero.Write(Socio[i].tam); //guarda el tamaño de byte que tiene la imagen.
                     fichero.Write(Socio[i].imagenSocio);
                 }
 
-                fichero.Close();
+                fichero.Close(); //Cerramos el fichero.
             }
             catch (Exception ex)
             {
-                MessageBox.Show("no guarda");
+                MessageBox.Show("No se puedo guardar"); //Un mensaje en caso de error.
             }
         }
 
         public void leerFichero()
         {
-            int NumeroSocioAux;
+            int NumeroSocioAux; //Declaro una variables auxiliares para leer el fichero.
             String NombreSocioAux;
             String ApellidoSocioAux;
             int EdadSocioAux;
@@ -232,11 +232,11 @@ namespace Abonados_betis2
 
             try
             {
-                BinaryReader Fichero2;
-                Fichero2 = new BinaryReader(File.OpenRead("databank.data"));
-                int count = Fichero2.ReadInt32();
-                lblNumeroTotalSocio.Text = count.ToString();
-                for (int i = 0; i < count; i++)
+                BinaryReader Fichero2; //Se crea un fichero para leer.
+                Fichero2 = new BinaryReader(File.OpenRead("databank.data")); //Se abre el fichero para leer su contenido.
+                int count = Fichero2.ReadInt32(); //leeo el numero total de registros guardados.
+                lblNumeroTotalSocio.Text = count.ToString(); //Actualizo el contador al numero de registros que haya.
+                for (int i = 0; i < count; i++) //Leemos los valores del fichero. Una vez termina creamos un abonado con las variables auxiliares.
                 {
                     NumeroSocioAux = Fichero2.ReadInt32();
                     NombreSocioAux = Fichero2.ReadString();
@@ -247,7 +247,7 @@ namespace Abonados_betis2
                     PagoSocioAux = Fichero2.ReadBoolean();
                     RutaImagenSocioAux = Fichero2.ReadString();
                     TamAux = Fichero2.ReadInt32();
-                    ImagenSocioAux = Fichero2.ReadBytes(TamAux);
+                    ImagenSocioAux = Fichero2.ReadBytes(TamAux); //Leemos los bytes que me indica el tamaño.
 
                     Abonados abonados = new Abonados();
                     abonados.numeroSocio = NumeroSocioAux;
@@ -260,46 +260,46 @@ namespace Abonados_betis2
                     abonados.rutaImagenSocio = RutaImagenSocioAux;
                     abonados.imagenSocio = ImagenSocioAux;
 
-                    Socio.Add(abonados);
-                    MostrarActual();
+                    Socio.Add(abonados); //Añado mi nuevo registro.
+                    MostrarActual();  //Muestro el actual.
                 }
-                Fichero2.Close();
+                Fichero2.Close(); //Cierro el Fichero.
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Mariano");
+                MessageBox.Show("No hay valores que leer en el fichero \no no se pudo abrir el fichero"); //Un mensaje en caso de error.
             }
         }
 
         private void butGuardar_Click(object sender, EventArgs e)
         {
-            gardarEnFichero();
+            gardarEnFichero(); //llamo al metodo para guardar el fichero.
         }
 
         private void butCargar_Click(object sender, EventArgs e)
         {
-            leerFichero();
+            leerFichero(); //llamo al metodo para leer el fichero.
         }
 
         private void comprobarbotones()
         {
-            if(posicion == 0)
+            if (posicion == 0) //Si es la primera posicion desactivo el boton atras.
             {
                 butAnterior.Enabled = false;
             }
-            else
+            else //Si no se activa.
             {
                 butAnterior.Enabled = true;
             }
-            if (posicion+1 == Socio.Count)
+            if (posicion + 1 == Socio.Count) //Si la posicion +1 es igual al total de registros desactivo el boton siguiente.
             {
                 butSiguiente.Enabled = false;
             }
-            else
+            else //Si no se activa el boton
             {
                 butSiguiente.Enabled = true;
             }
-            if(Socio.Count == 0)
+            if (Socio.Count == 0) //si solo hay un reguistro o ninguno, ambos botones se desactivan.
             {
                 butAnterior.Enabled = false;
                 butSiguiente.Enabled = false;
